56a49946713873ebfae62b2494a5962810423a1d
u2008113935 <u2008113935@gmail.com>
2025-12-29 21:17:36 -0500

    Update 29dic25 21h17m

--- apppasteleriav04/Models/Local/SyncQueue.cs
+++ apppasteleriav04/Models/Local/SyncQueue.cs
@@ -1,10 +1,31 @@
-﻿susing System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace apppasteleriav04.Models.Local
 {
-    internal class _
+    public class SyncQueue
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string EntityType { get; set; } = string.Empty; // "Order", "Transaction", "Payment"
+
+        public int LocalEntityId { get; set; }
+
+        public string Operation { get; set; } = "INSERT"; // INSERT, UPDATE, DELETE
+
+        public string? JsonData { get; set; }
+
+        public bool IsSynced { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? SyncedAt { get; set; }
+
+        public int RetryCount { get; set; } = 0;
+
+        public string? ErrorMessage { get; set; }
+
+        public int Priority { get; set; } = 0; // 0=normal, 1=high, 2=critical
     }
 }
--- apppasteleriav04/obj/Debug/net10.0-android/apppasteleriav04.AssemblyInfo.cs
+++ apppasteleriav04/obj/Debug/net10.0-android/apppasteleriav04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apppasteleriav04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fab79d9859829770ef6bcd7d80a6086d82f104a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3f69bc6818ef356ee4db4435caddd886e5d42783")]
 [assembly: System.Reflection.AssemblyProductAttribute("apppasteleriav04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apppasteleriav04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
